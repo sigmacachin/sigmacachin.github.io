59667999805b5cf0b4b164d149aa05c356efcd61
sigmacachin <frandeicas@gmail.com>
2020-08-07 14:26:36 -0300

    primera edicion

---
+++ data/Puntomvil_8.js
@@ -0,0 +1,22 @@
+var json_Puntomvil_8 = {
+"type": "FeatureCollection",
+"name": "Puntomvil_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Puesto": "1- Plurianual nuevo" }, "geometry": { "type": "Point", "coordinates": [ -63.669124490871653, -37.144123165719385 ] } },
+{ "type": "Feature", "properties": { "Puesto": "2- Salinas" }, "geometry": { "type": "Point", "coordinates": [ -63.665975262658321, -37.143909795984968 ] } },
+{ "type": "Feature", "properties": { "Puesto": "3- Avenida Roca" }, "geometry": { "type": "Point", "coordinates": [ -63.660506905490635, -37.138897728215575 ] } },
+{ "type": "Feature", "properties": { "Puesto": "4- Barrio Municipal" }, "geometry": { "type": "Point", "coordinates": [ -63.661195778823512, -37.135885501686666 ] } },
+{ "type": "Feature", "properties": { "Puesto": "5- Barrio Islas Malvinas" }, "geometry": { "type": "Point", "coordinates": [ -63.661244339485023, -37.132563199490363 ] } },
+{ "type": "Feature", "properties": { "Puesto": "6- Barrio Islas Malvinas" }, "geometry": { "type": "Point", "coordinates": [ -63.662802960023718, -37.131623204369113 ] } },
+{ "type": "Feature", "properties": { "Puesto": "8- Barrio Norte" }, "geometry": { "type": "Point", "coordinates": [ -63.665894354857187, -37.126848657888317 ] } },
+{ "type": "Feature", "properties": { "Puesto": "9- Barrio Los Sauces" }, "geometry": { "type": "Point", "coordinates": [ -63.668343594527009, -37.125672349969307 ] } },
+{ "type": "Feature", "properties": { "Puesto": "10- Barrio Los Sauces Oeste" }, "geometry": { "type": "Point", "coordinates": [ -63.675749310812257, -37.12818226540962 ] } },
+{ "type": "Feature", "properties": { "Puesto": "11- Plan 5000" }, "geometry": { "type": "Point", "coordinates": [ -63.673417809496591, -37.135976410529047 ] } },
+{ "type": "Feature", "properties": { "Puesto": "12- Plan 3000" }, "geometry": { "type": "Point", "coordinates": [ -63.672889123353102, -37.140232308865741 ] } },
+{ "type": "Feature", "properties": { "Puesto": "13- Terminaciones Mínimas" }, "geometry": { "type": "Point", "coordinates": [ -63.670735372816978, -37.14240444036124 ] } },
+{ "type": "Feature", "properties": { "Puesto": "14- Club Atlético Macachín" }, "geometry": { "type": "Point", "coordinates": [ -63.667785847467066, -37.141419173410462 ] } },
+{ "type": "Feature", "properties": { "Puesto": "15- Playón Terminal de Omnibus" }, "geometry": { "type": "Point", "coordinates": [ -63.666316607665237, -37.133482444946679 ] } },
+{ "type": "Feature", "properties": { "Puesto": "7- Barrio Islas Malvinas" }, "geometry": { "type": "Point", "coordinates": [ -63.665010410296084, -37.132578913948258 ] } }
+]
+}
---
+++ data/radios_2.js
@@ -0,0 +1,21 @@
+var json_radios_2 = {
+"type": "FeatureCollection",
+"name": "radios_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -63.6774283434235, -37.132526901030069 ], [ -63.655935346760934, -37.132647780364771 ] ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -63.666659555334917, -37.119825883131668 ], [ -63.666771495063252, -37.132587323612306 ] ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -63.677317167555472, -37.120052006655662 ], [ -63.67742842561146, -37.132536120379257 ] ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -63.666811848679714, -37.141417316154389 ], [ -63.666984769551583, -37.161107418635261 ] ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -63.681354209412866, -37.141334612958666 ], [ -63.656010796528477, -37.141477587546262 ] ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -63.666889055618242, -37.150211066382745 ], [ -63.656086753085546, -37.150271356876814 ] ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -63.661167353040135, -37.132618713775628 ], [ -63.661244352642136, -37.141466547198327 ] ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -63.6814411847961, -37.151029332541832 ], [ -63.666896968058495, -37.151112064635122 ] ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -63.672195575850544, -37.132556669866872 ], [ -63.672273700094308, -37.141386463535845 ] ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -63.684168989480931, -37.123303813236234 ], [ -63.665711235078597, -37.123408898557308 ] ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -63.665711235078597, -37.123408898557308 ], [ -63.661039467686564, -37.123721573503389 ] ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -63.655858561993462, -37.12375034923776 ], [ -63.655935346760934, -37.132647780364771 ] ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -63.655935346760934, -37.132647780364771 ], [ -63.656181701951787, -37.16116772845816 ] ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -63.661039467686564, -37.123721573503389 ], [ -63.653819522890551, -37.12376161235408 ] ] ] } }
+]
+}
